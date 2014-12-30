98c6521aa2ca7a4637231454566bb8d3e302934b
katoy <youichikato@gmail.com>
2014-12-30 10:28:33 +0900

    layout

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -5,6 +5,9 @@
 //  Created by katoy on 2014/12/29.
 //  Copyright (c) 2014年 Youichi Kato. All rights reserved.
 //
+// See https://www.youtube.com/watch?v=j35eYoxieUw
+//     https://www.youtube.com/watch?v=DGt1yBxBw9k
+//
 
 import UIKit
 
@@ -20,6 +23,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func btnNumber(sender: UIButton) {
+        println(sender.titleLabel?.text!.toInt())
+    }
 
+    @IBAction func btnOperate(sender: UIButton) {
+        println(sender.titleLabel?.text!)
+    }
+    
+    @IBAction func btnClear(sender: UIButton) {
+        println(sender.titleLabel?.text!)
+    }
 }
 
